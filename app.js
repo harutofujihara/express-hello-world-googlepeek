5bdbfcbc61b9e60b67b20134b21d69e6cb4ae4cb
haruto.fujihara <haruto.fujihara@linecorp.com>
2023-08-26 20:46:12 +0900

    add admin

--- app.js
+++ app.js
@@ -4,22 +4,56 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static(__dirname+'/public'));
 
-app.get("/", (req, res) => res.sendFile(__dirname + '/public/search.html'));
+const fs = require("fs");
+
+app.get("/", (req, res) => {
+  const path = "public/isPeeking.txt";
+
+  fs.readFile(path, "utf8", function(err, data){
+    if(err) {
+      res.send("isPeeking.txtの読み込み中にエラーが発生しました。");
+      return;
+    }
+    console.log(data.trim());
+    if (data.trim() == "true") {
+      res.sendFile(__dirname + '/public/search.html');
+    } else {
+      res.redirect("https://images.google.co.jp/");
+    }
+  });
+});
+
 app.get("/search", (req, res) => {
-  const fs = require("fs");
   const searchQuery = req.query.q;
   const data = searchQuery + " " + new Date() + "\n";
   // 書き込み
   fs.appendFile("public/queries.txt", data, (err) => {
-    if (err) throw err;
-    // console.log('正常に書き込みが完了しました');
+    if (err) {
+      res.send("クエリの記録に失敗しました。");
+      return;
+    }
   });
   res.redirect("https://www.google.co.jp/search?q=" + searchQuery + "&tbm=isch")
 });
-app.get("/admin", (req, res) => {
+
+app.get("/admin", (req, res) => res.sendFile(__dirname + '/public/admin.html'));
+app.get("/admin/peek", (req, res) => res.sendFile(__dirname + '/public/peek-check.html'));
+app.get("/admin/peek/able", (req, res) => {
+  fs.writeFile("public/isPeeking.txt", "true", () => {});
+    res.send("ok");
+  });
+app.get("/admin/peek/disable", (req, res) => {
+  fs.writeFile("public/isPeeking.txt", "false", () => {});
+  res.send("/で本物のgoogle画像検索にリダイレクトされます");
+});
+
+app.get("/admin/queries", (req, res) => {
   const fs = require("fs");
   fs.readFile("public/queries.txt", "utf8", function(err, data){
-    if(err) throw err;
+    if(err) {
+      res.send("queries.txtの読み込み中にエラーが発生しました。");
+      return;
+    }
 
     var lines = data.toString().split('\n');
 
